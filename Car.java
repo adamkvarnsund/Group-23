dbcce62a07090b7f4fa1cbddb0b4689de0c74b11
Adam Kvarnsund <adam.kvarnsund17@gmail.com>
2022-11-10 15:05:09 +0100

    Co-authored-by: DuchessMuhr <DuchessMuhr@users.noreply.github.com> Co-authored-by: Snebabe <Snebabe@users.noreply.github.com>

---
+++ Car.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+
+abstract class Car {
+    private int nrDoors; // Number of doors on the car
+    private double enginePower; // Engine power of the car
+    private double currentSpeed; // The current speed of the car
+    private Color color; // Color of the car
+    private String modelName; // The car model name
+
+
+    public Car(int nrDoors, double enginePower, Color color, String modelName){
+        this.nrDoors = nrDoors;
+        this.enginePower = enginePower;
+        this.color = color;
+        this.modelName = modelName;
+        stopEngine();
+    }
+
+    
+    void stopEngine() {
+        currentSpeed = 0;
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+    
+    public Color getColor(){
+        return color;
+    }
+}
--- Volvo240.java
+++ Volvo240.java
@@ -1,20 +1,11 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Car{
 
-    public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
+    // public final static double trimFactor = 1.25;
     
     public Volvo240(){
-        nrDoors = 4;
-        color = Color.black;
-        enginePower = 100;
-        modelName = "Volvo240";
-        stopEngine();
+        super(2, 1.5, Color.red, "snabb-bil");
     }
     
     public int getNrDoors(){
